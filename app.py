215c286b469f5cf26b8cfdedf6002aa466ef9290
Steven Chia <stevenchia56@gmail.com>
2020-01-27 19:00:30 +0800

    wadw

--- app.py
+++ app.py
@@ -48,11 +48,10 @@ def respond():
     text = update.message.text.encode('utf-8').decode()
     # for debugging purposes only
     print("got text message :", text)
-    # if context_tracker:
-    #     conversation(chat_id)
-    # else:
-    #     first_conversation(chat_id)
-    bot.sendMessage(chat_id=chat_id, text=message)
+    if context_tracker:
+        conversation(chat_id)
+    else:
+        first_conversation(chat_id)
     return 'ok'
 
 @app.route('/set_webhook', methods=['GET', 'POST'])

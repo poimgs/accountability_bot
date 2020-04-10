f65ff89a0105039066e16f0edd6e7ee7005417ab
Steven Chia <stevenchia56@gmail.com>
2020-04-11 00:01:48 +0800

    check if there are any mistakes

--- scheduled_messages.py
+++ scheduled_messages.py
@@ -8,25 +8,28 @@ TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
 def keep_accountable():
-    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-    cursor = connection.cursor()
+    try:
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = connection.cursor()
 
-    get_history_query = "SELECT * FROM accountability ORDER BY id DESC LIMIT 1"
-    cursor.execute(get_history_query)
-    rows = cursor.fetchall()
+        get_history_query = "SELECT * FROM accountability ORDER BY id DESC LIMIT 1"
+        cursor.execute(get_history_query)
+        rows = cursor.fetchall()
 
-    connection.commit()
-    connection.close()
+        connection.commit()
+        connection.close()
 
-    last_timestamp = row[0][1]
-    last_timestamp_modified = datetime.datetime(last_timestamp.year,last_timestamp.month,last_timestamp.day,last_timestamp.hour)
+        last_timestamp = row[0][1]
+        last_timestamp_modified = datetime.datetime(last_timestamp.year,last_timestamp.month,last_timestamp.day,last_timestamp.hour)
 
-    now = datetime.datetime.now()
-    now_modified = datetime.datetime(now.year,now.month,now.day,now.hour)
-    one_hour_before = now_modified - datetime.timedelta(hours=1)
+        now = datetime.datetime.now()
+        now_modified = datetime.datetime(now.year,now.month,now.day,now.hour)
+        one_hour_before = now_modified - datetime.timedelta(hours=1)
 
-    if last_timestamp_modified == one_hour_before:
-        bot.sendMessage(chat_id=247547763, text='What is your current choice?')
+        if last_timestamp_modified == one_hour_before:
+            bot.sendMessage(chat_id=247547763, text='What is your current choice?')
+    except:
+        bot.sendMessage(chat_id=247547763, text='I think something went wrong here')
 
 if __name__ == '__main__':
     keep_accountable()

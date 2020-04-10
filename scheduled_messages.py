af1c73caad1451eb30a12d9a1d362f101fdbbecf
Steven Chia <stevenchia56@gmail.com>
2020-04-10 23:53:59 +0800

    only send new messages when Steven has not replied bot

--- scheduled_messages.py
+++ scheduled_messages.py
@@ -1,12 +1,32 @@
 import telegram 
 from telebot.credentials import bot_token, bot_user_name, URL
+import datetime
+import os
+import psycopg2
+
+TOKEN = bot_token
+bot = telegram.Bot(token=TOKEN)
 
 def keep_accountable():
-    TOKEN = bot_token
-    bot = telegram.Bot(token=TOKEN)
+    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = connection.cursor()
 
-    chat_id = 247547763
-    bot.sendMessage(chat_id=chat_id, text='What is your current choice?')
+    get_history_query = "SELECT * FROM accountability ORDER BY id DESC LIMIT 1"
+    cursor.execute(get_history_query)
+    rows = cursor.fetchall()
+
+    connection.commit()
+    connection.close()
+
+    last_timestamp = row[0][1]
+    last_timestamp_modified = datetime.datetime(last_timestamp.year,last_timestamp.month,last_timestamp.day,last_timestamp.hour)
+
+    now = datetime.datetime.now()
+    now_modified = datetime.datetime(now.year,now.month,now.day,now.hour)
+    one_hour_before = now_modified - datetime.timedelta(hours=1)
+
+    if last_timestamp_modified == one_hour_before:
+        bot.sendMessage(chat_id=247547763, text='What is your current choice?')
 
 if __name__ == '__main__':
     keep_accountable()

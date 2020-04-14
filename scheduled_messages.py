71eae012ab528562db22c197a8d03c87677e5213
Steven Chia <stevenchia56@gmail.com>
2020-04-15 02:40:37 +0800

    fix bug on timing

--- scheduled_messages.py
+++ scheduled_messages.py
@@ -28,16 +28,16 @@ def keep_accountable():
     now_modified = datetime.datetime(now.year,now.month,now.day,now.hour)
     one_hour_before = now_modified - datetime.timedelta(hours=1)
 
-    one_day_later = now_modified + datetime.timedelta(days=1)
+    one_day_before = now_modified - datetime.timedelta(days=1)
 
     print(last_timestamp_modified)
     print(one_hour_before)
-    print(one_day_later)
+    print(one_day_before)
 
     if last_timestamp_modified == one_hour_before:
         bot.sendMessage(chat_id=247547763, text='What is your current choice?')
     
-    elif last_timestamp_modified == one_day_later:
+    elif last_timestamp_modified == one_day_before:
         bot.sendMessage(chat_id=247547763, text='Hey, it\'s been a day since you last updated me, seems like you are crashing or have crashed')
         bot.sendMessage(chat_id=247547763, text='It\'s time to seek help! :)')
 
